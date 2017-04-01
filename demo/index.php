a5a5b4ba3a464169578f7bd7b2e77f407de2aa65
sasou <zaipd@qq.com>
2017-04-01 10:28:28 +0800

    add 异常处理钩子；

--- demo/index.php
+++ demo/index.php
@@ -2,6 +2,10 @@
 header("Content-type: text/html; charset=UTF-8");
 define('APP_ROOT', __dir__ . '/app/');
 
+function geneHandler($e) {
+	\Gene\Router::displayExt('error');
+}
+
 $app = new \gene\application();
 $app
 ->load("router.ini.php")
