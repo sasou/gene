e30c6c51f93beecfbe9802fbc4594a4b41c763c7
sasou <zaipd@qq.com>
2018-01-22 22:13:57 +0800

    修改文档；

--- demo/app/Config/router.ini.php
+++ demo/app/Config/router.ini.php
@@ -4,15 +4,16 @@ $router->clear()
 ->get("/",function (){
         (new \Controllers\Index)->run();
     })
+
 ->group("/admin")
     ->get("/",function(){
         echo 'admin';
     },"auth")
-    ->get("/:name/",function($abc){
-        var_dump($abc);
+    ->get("/:name/",function($params){
+        var_dump($params);
     },"auth@")
-    ->get("/:name.:ext",function($abc){
-        var_dump($abc);
+    ->get("/:name.:ext",function($params){
+        var_dump($params);
     })
     ->get("/:name/sasoud",function(){
         echo 'dd';
@@ -22,20 +23,24 @@ $router->clear()
         return array('sdfasd'=>'baidu.edu.com');
     },"auth@clearAll")
 ->group()
+
 ->get("/index",function(){
     echo 'index';
 },"@clearAll")
+
+->get("/:baidu",function($params){
+    echo 'baidu';
+},"@clearAll")
+
+->get("/:c/:a.html","Controllers\\\:c@:a","@clearAll")
+
 ->error(404,function(){echo " 404 "; })
-->error("exception",function($e){
-    var_dump($e);
-    gene_router::display("error");
-})
-->hook("auth",function(){echo " auth ";})
+
+->hook("auth",function(){echo " auth-hook ";})
 ->hook("before", function(){
-    echo " before ";
-    $bbc = ' bbc ';
+    echo " before-hook ";
 })
 ->hook("after", function($params){
-    echo " after ";
+    echo " after-hook ";
     if(is_array($params))var_dump($params);
 });
