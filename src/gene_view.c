97cb67562b536b904d5c89805d1964bcc8c809f0
sasou2008 <sasou2008@163.com>
2018-12-28 11:35:41 +0800

    fixed build for php7.3;

--- src/gene_view.c
+++ src/gene_view.c
@@ -143,8 +143,9 @@ static int check_folder_exists(char *fullpath) {
  * {{{ gene_view
  */
 static int parser_templates(php_stream **stream, char *compile_path) {
-	int result_len, i;
-	zend_string *arg,*ret,*replace_val;
+	size_t result_len;
+	int i;
+	zend_string *arg,*ret;
 	php_stream *CacheStream = NULL;
 	char *subject = NULL,*result = NULL;
 
@@ -200,15 +201,30 @@ static int parser_templates(php_stream **stream, char *compile_path) {
 
 	for (i = 0; i < PARSER_NUMS; i++) {
 		arg = zend_string_init(regex[i], strlen(regex[i]), 0);
-		replace_val = zend_string_init(replace[i], strlen(replace[i]), 0);
-		if ((ret = php_pcre_replace(arg, NULL, result, result_len, replace_val, -1, &result_len)) != NULL) {
+
+#if PHP_VERSION_ID < 70200
+		zval replace_val;
+		ZVAL_STRINGL(&replace_val, replace[i], strlen(replace[i]));
+		if ((ret = php_pcre_replace(arg, NULL, result, result_len, &replace_val, 0, -1, &result_len)) != NULL) {
 			efree(result);
 			result = estrndup(ret->val, ret->len);
 			result_len = ret->len;
 			zend_string_free(ret);
 		}
 		zend_string_free(arg);
-		zend_string_free(replace_val);
+		zval_ptr_dtor(&replace_val);
+#else
+	zend_string *replace_val;
+	replace_val = zend_string_init(replace[i], strlen(replace[i]), 0);
+	if ((ret = php_pcre_replace(arg, NULL, result, result_len, replace_val, -1, &result_len)) != NULL) {
+		efree(result);
+		result = estrndup(ret->val, ret->len);
+		result_len = ret->len;
+		zend_string_free(ret);
+	}
+	zend_string_free(arg);
+	zend_string_free(replace_val);
+#endif
 	}
 
 	CacheStream = php_stream_open_wrapper(compile_path, "wb", REPORT_ERRORS, NULL);
