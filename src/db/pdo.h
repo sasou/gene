7bfbf346f34219914d2247e20f0290b1bd814579
sasou2008 <sasou2008@163.com>
2019-12-02 17:14:26 +0800

    fixed php7.1 nmake;

--- src/db/pdo.h
+++ src/db/pdo.h
@@ -17,6 +17,8 @@
 #ifndef GENE_DB_PDO_H
 #define GENE_DB_PDO_H
 
+#include "zend_smart_str_public.h"
+
 void array_to_string(zval *array, char **result);
 void mssql_array_to_string(zval *array, char **result);
 void gene_pdo_construct(zval *pdo_object, zval *dsn, zval *user, zval *pass, zval *options);
@@ -38,14 +40,15 @@ void gene_pdo_statement_fetch_object(zval *pdostatement_obj, zval *retval);
 void gene_pdo_statement_row_count(zval *pdostatement_obj, zval *retval);
 void gene_pdo_statement_set_fetch_mode(zval *pdostatement_obj, int fetch_style, zval *retval);
 void jsonEncode(zval *data, zval *param);
-void gene_mssql_insert_field_value (zval *fields, smart_str *field_str, smart_str *value_str,zval *field_value);
-void gene_mssql_insert_field_value_batch (zval *fields, smart_str *field_str, smart_str *value_str, zval *field_value);
-void gene_mssql_update_field_value (zval *fields, smart_str *field_str, zval *field_value);
+
+void gene_mssql_insert_field_value(zval *fields, smart_str *field_str, smart_str *value_str, zval *field_value);
+void gene_mssql_insert_field_value_batch(zval *fields, smart_str *field_str, smart_str *value_str, zval *field_value);
+void gene_mssql_update_field_value(zval *fields, smart_str *field_str, zval *field_value);
 void mssqlMakeWhere(zval *self, smart_str *where_str, zval *where, zval *field_value);
 void gene_insert_field_value (zval *fields, smart_str *field_str, smart_str *value_str,zval *field_value);
-void gene_insert_field_value_batch (zval *fields, smart_str *field_str, smart_str *value_str, zval *field_value);
-void gene_insert_field_value_batch_other (zval *fields, smart_str *value_str, zval *field_value);
-void gene_update_field_value (zval *fields, smart_str *field_str, zval *field_value);
+void gene_insert_field_value_batch(zval *fields, smart_str *field_str, smart_str *value_str, zval *field_value);
+void gene_insert_field_value_batch_other(zval *fields, smart_str *value_str, zval *field_value);
+void gene_update_field_value(zval *fields, smart_str *field_str, zval *field_value);
 void makeWhere(zval *self, smart_str *where_str, zval *where, zval *field_value);
 zend_bool checkPdoError(zend_object *ex);
 
