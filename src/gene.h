024393b804aa8595923ec9f0f22ffbeede849017
sasou2008 <sasou2008@163.com>
2019-12-10 15:27:06 +0800

    适配php7.4.0;

--- src/gene.h
+++ src/gene.h
@@ -20,7 +20,7 @@
 extern zend_module_entry gene_module_entry;
 #define phpext_gene_ptr &gene_module_entry
 
-#define PHP_GENE_VERSION "3.2.1"
+#define PHP_GENE_VERSION "3.2.2"
 
 #ifdef PHP_WIN32
 #	define PHP_GENE_API __declspec(dllexport)
