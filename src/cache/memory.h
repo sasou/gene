d3e5a9fac530d9da8c519ea97b0c79047783c9ce
sasou <zaipd@qq.com>
2024-10-30 20:41:42 +0800

    update;

--- src/cache/memory.h
+++ src/cache/memory.h
@@ -36,7 +36,7 @@ typedef struct _filenode filenode;
 
 extern zend_class_entry *gene_memory_ce;
 
-void gene_memory_init(TSRMLS_D);
+void gene_memory_init();
 void gene_hash_destroy(HashTable *ht);
 void gene_memory_set(char *keyString, size_t keyString_len, zval *zvalue, int validity);
 zval * gene_memory_get(char *keyString, size_t keyString_len);
