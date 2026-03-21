bb10e03860f70c2f9c01e2b8f57c5e5096fa47dd
sasou <zaipd@qq.com>
2026-03-21 19:51:24 +0800

    fixed;

--- src/db/pool.c
+++ src/db/pool.c
@@ -518,13 +518,15 @@ PHP_METHOD(gene_pool, create)
         return;
     }
 
-    /* --- Read DB config from persistent cache (same path as gene_di_get) --- */
+    /* --- Read DB config from persistent cache (same path as Gene\Config) --- */
     char *router_e = NULL;
     size_t router_e_len = 0;
-    if (GENE_G(app_key)) {
+    if (GENE_G(app_key) && strlen(GENE_G(app_key)) > 0) {
         router_e_len = spprintf(&router_e, 0, "%s%s", GENE_G(app_key), GENE_CONFIG_CACHE);
-    } else {
+    } else if (GENE_G(directory) && strlen(GENE_G(directory)) > 0) {
         router_e_len = spprintf(&router_e, 0, "%s%s", GENE_G(directory), GENE_CONFIG_CACHE);
+    } else {
+        router_e_len = spprintf(&router_e, 0, "%s", GENE_CONFIG_CACHE);
     }
 
     zval *di_config = gene_memory_get_by_config(router_e, router_e_len, config_key);
